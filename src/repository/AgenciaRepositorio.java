b7937e97c900a3705d8859924f6cb4a3b900089a
Isaque Menezes <isaquesantos1517@gmail.com>
2024-09-20 14:19:00 -0300

    feat: add agency repository class and its features

---
+++ src/repository/AgenciaRepositorio.java
@@ -0,0 +1,41 @@
+package repository;
+
+import entities.agencia.Agencia;
+import exception.InstanciaInvalidaException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AgenciaRepositorio extends Repositorio {
+
+    private final List<Agencia> agencias;
+
+    public AgenciaRepositorio() {
+        this.agencias = new ArrayList<Agencia>();
+    }
+
+    @Override
+    public Agencia getOne() {
+        return agencias.getFirst();
+    }
+
+    @Override
+    public List<Agencia> getAll() {
+        return agencias;
+    }
+
+    @Override
+    public Agencia add(Object objeto) throws InstanciaInvalidaException {
+        if (objeto instanceof Agencia agencia) {
+            agencias.add(agencia);
+            return agencia;
+        }
+        throw new InstanciaInvalidaException("Instancia invalida, necessário uma agencia.");
+    }
+
+    @Override
+    public Agencia delete(Object id) {
+        return null;
+    }
+
+}
