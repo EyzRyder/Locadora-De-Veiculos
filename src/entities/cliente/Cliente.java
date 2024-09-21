305854f6e699d13e6bfb95c7de4cd2e56354758f
Isaque Menezes <isaquesantos1517@gmail.com>
2024-09-21 16:15:40 -0300

    refactor: add getters and setters in client class

--- src/entities/cliente/Cliente.java
+++ src/entities/cliente/Cliente.java
@@ -6,7 +6,6 @@ public class Cliente {
     protected String email;
     protected String senha;
 
-
     public Cliente(String nome, String telefone, String email, String senha) {
         this.nome = nome;
         this.telefone = telefone;
@@ -17,6 +16,23 @@ public class Cliente {
     public String getEmail() {
         return email;
     }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
 
     public boolean verificarSenha(String senha) {
         return this.senha.equals(senha);
