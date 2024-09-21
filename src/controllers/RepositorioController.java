a9773857b4506b3a9a8ce3188b34ce26bd580c50
Isaque Menezes <isaquesantos1517@gmail.com>
2024-09-21 16:20:09 -0300

    refactor: add agency and vehicle repository in repository controller

--- src/controllers/RepositorioController.java
+++ src/controllers/RepositorioController.java
@@ -1,11 +1,19 @@
 package controllers;
 
+import repository.AgenciaRepositorio;
 import repository.ClienteRepositorio;
+import repository.VeiculoRepositorio;
 
 public class RepositorioController {
+
     public ClienteRepositorio clientes;
+    public AgenciaRepositorio agencias;
+    public VeiculoRepositorio veiculos;
 
     public RepositorioController() {
         this.clientes = new ClienteRepositorio();
+        this.agencias = new AgenciaRepositorio();
+        this.veiculos = new VeiculoRepositorio();
     }
+
 }
