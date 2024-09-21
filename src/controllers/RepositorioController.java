eaadb7ef91ac4ddeda1159ff38e2bde697826203
Isaque Menezes <isaquesantos1517@gmail.com>
2024-09-21 16:25:01 -0300

    refactor: add client control methods

--- src/controllers/RepositorioController.java
+++ src/controllers/RepositorioController.java
@@ -1,8 +1,10 @@
 package controllers;
 
+import entities.cliente.Cliente;
 import repository.AgenciaRepositorio;
 import repository.ClienteRepositorio;
 import repository.VeiculoRepositorio;
+import util.ModoExibir;
 
 public class RepositorioController {
 
@@ -14,6 +16,24 @@ public class RepositorioController {
         this.clientes = new ClienteRepositorio();
         this.agencias = new AgenciaRepositorio();
         this.veiculos = new VeiculoRepositorio();
+    }
+
+    public ModoExibir adicionarCliente(Cliente cliente) {
+        if (cliente == null || clientes.find(cliente.getEmail()) != null) {
+            System.out.println("Cliente ja existe.");
+        } else {
+            clientes.add(cliente);
+        }
+        return ModoExibir.PRINCIPAL;
+    }
+
+    public ModoExibir alterarCliente(Cliente cliente) {
+        if (cliente == null || clientes.find(cliente.getEmail()) != null) {
+            System.out.println("Cliente inexistente.");
+        } else {
+            clientes.update(cliente.getEmail(), cliente);
+        }
+        return ModoExibir.PRINCIPAL;
     }
 
 }
